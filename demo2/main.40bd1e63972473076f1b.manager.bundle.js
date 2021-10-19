8fd6bab25b937f4606e6810972cece2c0c0bcda0
Pooya Paridel <ParidelPooya@users.noreply.github.com>
2021-10-19 15:23:16 -0700

    v1

---
+++ demo2/main.40bd1e63972473076f1b.manager.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{410:function(module,exports,__webpack_require__){__webpack_require__(411),__webpack_require__(788),module.exports=__webpack_require__(789)},478:function(module,exports){}},[[410,2,3]]]);
